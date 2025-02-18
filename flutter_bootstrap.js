89ec768790309a3acf60d8c5dc1e94b1485ad2e9
naneps <72372613+naneps@users.noreply.github.com>
2025-02-18 10:08:19 +0000

    Deploying to gh-pages from @ naneps/me@5636e48d57ca88bf639ff7dc5d82f65a0b097f4d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "808423916"
+    serviceWorkerVersion: "3276911388"
   }
 });
